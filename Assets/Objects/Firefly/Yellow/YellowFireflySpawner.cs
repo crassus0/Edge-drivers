0725401210b48729dec374e510aa4f95494539af
crassus <volodymyr.tsaruk@unrewealed.com>
2013-07-09 23:08:10 +0300

    Firefly cannon added

---
+++ Assets/Objects/Firefly/Yellow/YellowFireflySpawner.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using System.Collections;
+
+public class YellowFireflySpawner : ObjectSpawner
+{
+  public int spin=1;
+  public override void OnStart()
+  {
+    base.OnStart();
+    transform.Rotate(new Vector3(0, Direction * -60, 0));
+  }
+  protected override void OnUpdated()
+  {
+    base.OnUpdated();
+    if (step == 1)
+    {
+      transform.Rotate(new Vector3(0, spin * -60, 0));
+      Direction = Direction + spin;
+    }
+  }
+  protected new void OnDrawGizmos()
+  { 
+  }
+}

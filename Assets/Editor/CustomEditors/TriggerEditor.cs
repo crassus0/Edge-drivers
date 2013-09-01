aa0045595a16c1e57eb48c8b937659d8e4aa1157
crassus <volodymyr.tsaruk@fractionflow.com>
2013-09-01 13:52:27 +0300

    Bugs fixed

--- Assets/Editor/CustomEditors/TriggerEditor.cs
+++ Assets/Editor/CustomEditors/TriggerEditor.cs
@@ -7,8 +7,8 @@ using System.Collections.Generic;
 [CustomEditor(typeof(Trigger))]
 public class TriggerEditor : Editor
 {
-	int index;
-	string[] objectList=
+  int index;
+  string[] objectList =
 	{
 		"PlanerCore",
 		"RedFirefly",
@@ -20,24 +20,25 @@ public class TriggerEditor : Editor
 		"WebMine"
 		
 	};
-	int[] indexes;
-	void OnEnable()
-	{
-		Trigger targ = target as Trigger;
-		indexes=new int[objectList.Length];
-		for(int i=0; i<indexes.Length; i++)
-		{
-			indexes[i]=i;
-		}
-		List<string> list=new List<string>(objectList);
-		index=list.IndexOf(targ.TargetTriggerName);
-	}
-	public override void OnInspectorGUI ()
-	{
-		base.OnInspectorGUI ();
-		Trigger targ = target as Trigger;
-    index=EditorGUILayout.IntPopup("Trigger activator type", index, objectList, indexes);
-		targ.TargetTriggerName=objectList[index];
-		
-	}
+  int[] indexes;
+  void OnEnable()
+  {
+    Trigger targ = target as Trigger;
+    indexes = new int[objectList.Length];
+    for (int i = 0; i < indexes.Length; i++)
+    {
+      indexes[i] = i;
+    }
+    List<string> list = new List<string>(objectList);
+    index = list.IndexOf(targ.TargetTriggerName);
+  }
+  public override void OnInspectorGUI()
+  {
+    base.OnInspectorGUI();
+    Trigger targ = target as Trigger;
+    index = EditorGUILayout.IntPopup("Trigger activator type", index, objectList, indexes);
+    if (index >= 0)
+      targ.TargetTriggerName = objectList[index];
+
+  }
 }
